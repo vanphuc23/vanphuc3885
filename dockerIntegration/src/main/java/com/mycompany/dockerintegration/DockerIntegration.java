df1e58e33056c75eb57256a3f2011e234fb386d0
vanphuc23 <phucnvp15072k3@gmail.com>
2025-03-05 10:07:59 +0700

    git

---
+++ dockerIntegration/src/main/java/com/mycompany/dockerintegration/DockerIntegration.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dockerintegration;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class DockerIntegration {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
