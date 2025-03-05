5baecf9371d4b874ddf422563a5a8c2eac9a7151
vanphuc23 <phucnvp15072k3@gmail.com>
2025-03-05 10:24:54 +0700

    UpdateEmail

--- dockerIntegration/src/main/java/com/mycompany/dockerintegration/User.java
+++ dockerIntegration/src/main/java/com/mycompany/dockerintegration/User.java
@@ -9,5 +9,19 @@ package com.mycompany.dockerintegration;
  * @author ADMIN
  */
 public class User {
-    
+    private String userId;
+    private String email;
+
+    public User(String userId, String email) {
+        this.userId = userId;
+        this.email = email;
+    }
+
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public void updateEmail(String newEmail) { this.email = newEmail; }
+ 
 }
+
+//updateEmail
